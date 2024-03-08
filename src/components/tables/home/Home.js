06456015e867b06bf1b5c39707620313a00fc67b
anwar <anwarkp.qw@gmail.com>
2024-03-08 10:26:34 +0530

    Data table added needed modifications

--- src/components/tables/home/Home.js
+++ src/components/tables/home/Home.js
@@ -1,31 +1,32 @@
-// commented because it causing an error import DataTable from 'datatables.net-dt'
+import React from 'react'
+import JsonData from './Data.json'
 const Home = () => {
- // Sample data
- const data = [
-  { id: 1, name: 'John Doe', age: 30 },
-  { id: 2, name: 'Jane Smith', age: 25 },
-  // Add more data as needed
-];
-
-// Define columns
-const columns = [
-  { name: 'ID', selector: 'id', sortable: true },
-  { name: 'Name', selector: 'name', sortable: true },
-  { name: 'Age', selector: 'age', sortable: true },
-  // Add more columns as needed
-];
-
-
-
+  const DisplayData=JsonData.map(
+    (info)=>{
+        return(
+            <tr>
+                <td>{info.id}</td>
+                <td>{info.name}</td>
+                <td>{info.city}</td>
+            </tr>
+        )
+        })
   return (
     <div>
-      <p>table data</p>
-      <div>
-        fklkdlk
-        kf;l'kd;l'
-        
-      </div>
-  </div>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+            <th>Sr.NO</th>
+            <th>Name</th>
+            <th>City</th>
+            </tr>
+        </thead>
+        <tbody>
+            {DisplayData}
+        </tbody>
+    </table>
+</div>
   )
 }
+
 export default Home
